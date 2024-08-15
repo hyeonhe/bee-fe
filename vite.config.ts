e489fd76526772aabcdfec841abf6cf5789967d8
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-08-15 15:27:59 +0900

    chore: 폴더 경로 설정, server port 설정, 라이브러리 설치

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "assets", replacement: "/src/assets" },
+      { find: "pages", replacement: "/src/pages" },
+      { find: "components", replacement: "/src/components" },
+      { find: "api", replacement: "/src/api" },
+      { find: "utils", replacement: "/src/utils" },
+      { find: "store", replacement: "/src/store" },
+      { find: "hooks", replacement: "/src/hooks" },
+    ],
+  },
+  server: { port: 3000 },
 });
